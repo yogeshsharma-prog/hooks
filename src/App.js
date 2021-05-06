c15e9951d56f0cc48ea1ad2215863ac006000d09
Yogesh Sharma <yogeshsharma1196@gmail.com>
2021-05-06 09:13:35 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ClassCounterOne from "./Components/ClassCounterOne";
+import ClassMouse from "./Components/ClassMouse";
+import HookCounterOne from "./Components/HookCounterOne";
+import HookMouse from "./Components/HookMouse";
+import IntervalClassCounter from "./Components/IntervalClassCounter";
+import IntervalHookCounter from "./Components/IntervalHookCounter";
+import MouseContainer from "./Components/MouseContainer";
+import PostForm from "./Components/PostForm";
+import PostList from "./Components/PostList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IntervalHookCounter />
     </div>
   );
 }
